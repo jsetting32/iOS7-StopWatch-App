797b21b660df76132d09b920a513dea02d4f36ae
John Setting <jsetting32@gmail.com>
2014-09-11 02:57:16 -0700

    Initial Commit

---
+++ iOS7-StopWatch/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  iOS7-StopWatch
+//
+//  Created by John Setting on 9/11/14.
+//  Copyright (c) 2014 John Setting. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
